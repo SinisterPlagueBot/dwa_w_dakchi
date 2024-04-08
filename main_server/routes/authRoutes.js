7545553e6ec5c032d479d819a614ac267fea994b
SinisterPlagueBot <kasmimehdi561@gmail.com>
2024-04-08 23:35:08 +0200

    enhanced the process of sigin/signup ,TODO: session managemenet

--- main_server/routes/authRoutes.js
+++ main_server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const ClientController = require("../controllers/ClientController");
 const { ClientModel } = require("../models/Client");
 router.get("/signup", (req, res) => {
-  res.render("front_end/signup.ejs");
+  res.render("signup/signup.ejs");
 });
 
 router.post("/signup", (req, res) => {
