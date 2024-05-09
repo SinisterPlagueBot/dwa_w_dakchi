47cc4bb6829dc44eb5a37927251d3c61abe8bb03
SinisterPlagueBot <kasmimehdi561@gmail.com>
2024-05-09 23:29:27 +0200

    added covid model

--- ml_server/app.py
+++ ml_server/app.py
@@ -4,21 +4,35 @@ import numpy as np
 
 app = Flask(__name__)
 
-def preprocess(data, scaler):
+def preprocess_diabetes(data, scaler):
     data_array = np.array([data])
     data_array = scaler.transform(data_array)
     return data_array
 
-def load_model():
+def preprocess_covid(data, scaler):
+    data_array = np.array([data])
+    data_array = scaler.transform(data_array)
+    return data_array
+
+def load_model_diabetes():
     model_path = "diabetes.joblib"
     scaler_path = "scaler.joblib"
     model = joblib.load(model_path)
     scaler = joblib.load(scaler_path)
     return model, scaler
 
-model, scaler = load_model()
-
-@app.route("/predict", methods=["POST"])
+def load_model_covid():
+    model_path = "covid_model.joblib"
+    def preprocess_covid(data):
+        data = np.array(data)
+        data=np.where(data=='Yes',1,0)
+        return data.reshape(1,-1)
+        
+    model = joblib.load(model_path)
+    return model,preprocess_covid
+model_diabetes, scaler = load_model_diabetes()
+model_covid,preprocess_covid=load_model_covid()
+@app.route("/predict_diabetes", methods=["POST"])
 def predict_diabetes():
     data = request.json
     print(data)
@@ -31,12 +45,38 @@ def predict_diabetes():
         return jsonify({"error": f"Missing keys: {', '.join(missing_keys)}"}), 400
 
     values_list = [data[key] for key in expected_keys]
-    preprocessed_data = preprocess(values_list, scaler)
-    prediction = int(model.predict(preprocessed_data)[0])  
+    preprocessed_data = preprocess_diabetes(values_list, scaler)
+    prediction = int(model_diabetes.predict(preprocessed_data)[0])  
     print(prediction)
     if prediction == 0 :
         return jsonify({"diabetes_prediction": "Pas diabétique"})
     return jsonify({"diabetes_prediction": "Diabétique"})
+
+@app.route("/predict_covid", methods=["POST"])
+def predict_covid():
+    data = request.json
+    print(data)
+    if data is None:
+        return jsonify({"error": "No JSON data provided"}), 400
+
+    expected_keys = [
+    "Breathing Problem", "Fever", "Dry Cough", "Sore throat",  
+    "Hyper Tension", "Abroad travel", 
+    "Contact with COVID Patient", "Attended Large Gathering", 
+    "Visited Public Exposed Places", "Family working in Public Exposed Places"
+    
+]
+    missing_keys = [key for key in expected_keys if key not in data]
+    if missing_keys:
+        return jsonify({"error": f"Missing keys: {', '.join(missing_keys)}"}), 400
+
+    values_list = [data[key] for key in expected_keys]
+    preprocessed_data = preprocess_covid(values_list)
+    prediction = int(model_covid.predict(preprocessed_data))  
+    print(prediction)
+    if prediction == 0 :
+        return jsonify({"covid_prediction": "no covid"})
+    return jsonify({"covid_prediction": "covid detected"})
 
 
 @app.route("/")
--- ml_server/index.js
+++ ml_server/index.js
@@ -1,37 +1,49 @@
-import express from 'express';
-import axios from 'axios';
-import bodyParser from 'body-parser';
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
-const API_URL ="http://127.0.0.1:5000"
+const API_URL = "http://127.0.0.1:5000";
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
-
-app.get('/', (req, res) => {
-    res.render("index.html");
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
-app.post("/submit",async (req,res) => {
-    const formData = req.body;
-    console.log(formData);
-    try{
-        const response = await axios.post(API_URL+"/predict",formData);
-        console.log(response.data)
-        res.render("response.ejs",{data :response.data.diabetes_prediction});
-    }catch(err){
-        console.error("Error:", err);
-        res.status(500).send("Internal Server Error");
-    }
-} );
-
-
+app.post("/submit_diabetes", async (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  try {
+    const response = await axios.post(API_URL + "/predict_diabetes", formData);
+    console.log(response.data);
+    res.render("response_diabetes.ejs", {
+      data: response.data.diabetes_prediction,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.post("/submit_covid", async (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  try {
+    const response = await axios.post(API_URL + "/predict_covid", formData);
+    console.log(response.data);
+    res.render("response_covid.ejs", {
+      data: response.data.covid_prediction,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
